27203144932820f9d8786fc689cf772d6f1e7d8f
rogeriomussoco <rogeriotiagomiguelit@gmail.com>
2022-01-03 20:29:40 -0300

    atualizando oos dados

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports =  { 
+    "username": "postgres",
+    "password": "Null",
+    "database": "node-sequelize-do-zero",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    //"operatorsAliases": false,
+    "define": { 
+      "timestamps": true,
+      "underscored": true,
+      "underscoredAll": true
+    }  
+}
+
